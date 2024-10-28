61742e5133c38f526b97da03027ee8f750008e6d
Prince Papaya <topher@studiopuri.com>
2024-10-28 09:45:04 -0700

    trying to get crazy to work

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 import os
 import random
 
+
+
 import discord
 from discord import app_commands
 from dotenv import load_dotenv
@@ -13,7 +15,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 OPENAI_KEY = os.getenv('OPENAI_KEY')
 intents = discord.Intents.all()
 intents.message_content = True
-prefix = '/'
+prefix = '!'
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 client = discord.Client(intents=discord.Intents.all())
 tree = app_commands.CommandTree(client)
@@ -21,6 +23,20 @@ tree = app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if client.user.id != message.author.id:
+        if 'crazy' in message.content:
+            await message.channel.send('hmm.')
+            await client.send_message(message.channel, 'Crazy?')
+            await client.send_message(message.channel, 'I was crazy once.')
+            await client.send_message(message.channel, 'They locked me in a room.')
+            await client.send_message(message.channel, 'A rubber room.')
+            await client.send_message(message.channel, 'A rubber room with rats.')
+            await client.send_message(message.channel, 'Rats.')
+            await client.send_message(message.channel, 'Rats make me crazy.')
+    await client.process_commands(message)
 
 @bot.hybrid_command()
 async def anbennar(ctx):
