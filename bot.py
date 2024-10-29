95281373fdaa9343251e4396fa3d92752d70a2fe
Prince Papaya <topher@studiopuri.com>
2024-10-29 12:25:23 -0700

    made crazy copypasta more streamlined

--- bot.py
+++ bot.py
@@ -23,30 +23,13 @@ tree = app_commands.CommandTree(client)
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if client.user.id != message.author.id:
-        if 'crazy' in message.content:
-            await message.channel.send('hmm.')
-            await client.send_message(message.channel, 'Crazy?')
-            await client.send_message(message.channel, 'I was crazy once.')
-            await client.send_message(message.channel, 'They locked me in a room.')
-            await client.send_message(message.channel, 'A rubber room.')
-            await client.send_message(message.channel, 'A rubber room with rats.')
-            await client.send_message(message.channel, 'Rats.')
-            await client.send_message(message.channel, 'Rats make me crazy.')
-    await client.process_commands(message)
-
 @bot.event
 async def on_message(ctx):
     if 'crazy' in ctx.content:
         if bot.user != ctx.author:
-            await ctx.channel.send('Crazy?')
-            await ctx.channel.send('I was crazy once.')
-            await ctx.channel.send('They locked me in a room.')
-            await ctx.channel.send('A rubber room.')
-            await ctx.channel.send('A rubber room with rats.')
-            await ctx.channel.send('Rats.')
+            await ctx.channel.send('Crazy?\nI was crazy once.')
+            await ctx.channel.send('They locked me in a room.\nA rubber room.')
+            await ctx.channel.send('A rubber room with rats.\nRats.')
             await ctx.channel.send('Rats make me crazy.')
     await bot.tree.sync()
 
